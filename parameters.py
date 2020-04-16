a746c693c29ed3efd49d58ac1c95d5afb2e467db
Nikhil <nikhil.manu2000@gmail.com>
2020-04-16 15:26:54 +0530

    Display changes

--- parameters.py
+++ parameters.py
@@ -1,7 +1,9 @@
 import json
 import subprocess
 import os
-
+Name_len=0
+Value0_len=0
+Value1_len=0
 
 def get_services():
     services=[]
@@ -40,29 +42,51 @@ def get_parameters(service,environment):
 def comparsion(converted_parameter_json_dev1,converted_parameter_json_dev2):
     json_dev1_dict = dict()
     result_dict = dict()
+    global Name_len
+    global Value1_len
+    global Value0_len
 
     for i in converted_parameter_json_dev1["Parameters"]:
         json_dev1_dict[i['Name']] = [i['Value'], "Null"]
+        if len(json_dev1_dict[i['Name']])> Name_len:
+            Name_len=len(json_dev1_dict[i['Name']])
+        if len(json_dev1_dict.get(i['Name'])[0])> Value0_len:
+            Value0_len=len(json_dev1_dict.get(i['Name'])[0])
+
     for j in converted_parameter_json_dev2["Parameters"]:
         if j['Name'] not in json_dev1_dict.keys():
             result_dict[j['Name']] = ["Null", j['Value']]
+        if len(j['Name'])>Name_len:
+            Name_len=len(j['Name'])
+        if len(j['Value'])>Value1_len:
+            Value1_len=len(j['Value'])
         elif j['Name'] in json_dev1_dict.keys():
+            if len(j['Name'])>Name_len:
+                Name_len=len(j['Name'])
             if j['Value'] != json_dev1_dict.get(j['Name'])[0]:
                 result_dict[j['Name']] = [json_dev1_dict.get(j['Name'])[0], j['Value']]
+                if len(j['Value'])>Value1_len:
+                    Value1_len=len(j['Value'])
+                if len(json_dev1_dict.get(j['Name'])[0])>Value0_len:
+                    Value0_len=len(json_dev1_dict.get(j['Name'])[0])
             del json_dev1_dict[j['Name']]
     result_dict.update(json_dev1_dict)
     return result_dict
 
 
 def data_display(resultant_dict,env1,env2):
-    f=open("output.txt","w")
-    f.write("|{:<70}| {:<180} |{:<150} ".format('Name', env1, env2))
+    global Name_len
+    global Value0_len
+    global Value1_len
+
+    f=open("output.txt","w+")
+    formatter = "|{:<" + str(Name_len) + "}| {:<" + str(Value0_len) + "} |{:<" + str(Value1_len) + "} "
+    f.write(formatter.format('Name', env1, env2))
     f.write("\n")
     for Name, Value in resultant_dict.items():
-        f.write("|{:<70}| {:<180} |{:<150} ".format(Name, Value[0], Value[1]))
+        f.write(formatter.format(Name, Value[0], Value[1]))
         f.write("\n")
     f.close()
-    subprocess.run(['gedit', 'output.txt'])
 
 
 if __name__ == "__main__":
@@ -77,4 +101,5 @@ if __name__ == "__main__":
         conv_json_dev2 = get_parameters(serv,env2)
         result_dict = comparsion(conv_json_dev1,conv_json_dev2)
         data_display(result_dict,env1,env2)
+    subprocess.run(['gedit','output.txt'])
 
